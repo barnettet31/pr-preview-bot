90b5db38a5b3948bfcfc7f30ca75b5213ee3ee27
Travis Barnette <barnette.travis31@gmail.com>
2025-11-11 11:07:23 -0500

    add more robust error handling around namespaces

--- src/k8s/preview.ts
+++ src/k8s/preview.ts
@@ -8,77 +8,97 @@ interface IDeployParams {
 export const deployPreview = async ({ namespace, hostname }: IDeployParams) => {
     try {
         await k8sApi.readNamespace({ name: namespace });
+        console.log("Namespace already exists, skipping deployment")
         return;
     } catch (e: any) {
         if (e.statusCode !== 404) throw e;
     }
-    await k8sApi.createNamespace({ body: { metadata: { name: namespace } } });
-    await k8sApps.createNamespacedDeployment({
-        namespace,
-        body: {
-            metadata: {
-                name: 'preview-app'
-            },
-            spec: {
-                replicas: 1,
-                selector: { matchLabels: { app: 'preview-app' } },
-                template: {
-                    metadata: {
-                        labels: { app: 'preview-app' },
+    try {
 
-                    }, spec: {
-                        containers: [{
-                            name: 'preview-app',
-                            image: 'barnettet31/k3s-demo:v1',
-                            ports: [{ containerPort: 3000 }]
-                        }]
-                    },
+        await k8sApi.createNamespace({ body: { metadata: { name: namespace } } });
+        await k8sApps.createNamespacedDeployment({
+            namespace,
+            body: {
+                metadata: {
+                    name: 'preview-app'
+                },
+                spec: {
+                    replicas: 1,
+                    selector: { matchLabels: { app: 'preview-app' } },
+                    template: {
+                        metadata: {
+                            labels: { app: 'preview-app' },
+
+                        }, spec: {
+                            containers: [{
+                                name: 'preview-app',
+                                image: 'barnettet31/k3s-demo:v1',
+                                ports: [{ containerPort: 3000 }]
+                            }]
+                        },
+                    }
                 }
             }
-        }
-    });
-    await k8sApi.createNamespacedService({
-        namespace,
-        body: {
-            metadata: {
-                name: 'preview-app'
-            },
-            spec: {
-                type: 'NodePort',
-                selector: {
-                    app: 'preview-app'
+        });
+        await k8sApi.createNamespacedService({
+            namespace,
+            body: {
+                metadata: {
+                    name: 'preview-app'
                 },
-                ports: [{ port: 3000, targetPort: 3000 }],
+                spec: {
+                    type: 'NodePort',
+                    selector: {
+                        app: 'preview-app'
+                    },
+                    ports: [{ port: 3000, targetPort: 3000 }],
 
+                }
             }
-        }
-    });
-    await k8sNet.createNamespacedIngress({
-        namespace,
-        body: {
-            metadata: {
-                name: 'preview-ingress'
-            },
-            spec: {
-                ingressClassName: 'traefik',
-                rules: [{
-                    host: hostname, http: {
-                        paths: [{
-                            path: '/', pathType: 'Prefix', backend: {
-                                service: {
-                                    name: "preview-app",
-                                    port: { number: 3000 }
+        });
+        await k8sNet.createNamespacedIngress({
+            namespace,
+            body: {
+                metadata: {
+                    name: 'preview-ingress'
+                },
+                spec: {
+                    ingressClassName: 'traefik',
+                    rules: [{
+                        host: hostname, http: {
+                            paths: [{
+                                path: '/', pathType: 'Prefix', backend: {
+                                    service: {
+                                        name: "preview-app",
+                                        port: { number: 3000 }
+                                    }
                                 }
-                            }
-                        }]
-                    }
-                }]
+                            }]
+                        }
+                    }]
+                }
             }
+        })
+    } catch (e: any) {
+        console.error(`Failed to deploy PR, cleaning up:`, e.body?.message || e.message);
+        try {
+            await deletePreview({ namespace, hostname });
+        } catch (e: any) {
+            console.error("Failed to cleanup", e);
         }
-    })
+    }
 }
 
 export const deletePreview = async ({ namespace }: IDeployParams) => {
-    await k8sApi.deleteNamespace({ name: namespace, })
+    try {
+
+        await k8sApi.deleteNamespace({ name: namespace, })
+    } catch (e: any) {
+        if (e.statusCode === 404) {
+            console.error("Namespace already deleted");
+            return;
+        }
+        throw e;
+    }
 
 }
